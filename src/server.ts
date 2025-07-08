e63c9af2bfa09276257a0ee8d0e5b1f4bf3b8265
Thiago Leite <thiagofmleite@gmail.com>
2025-07-08 19:56:08 -0300

    refactor: clean up server.ts by removing redundant import and simplifying server start logic

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
+import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
-import { fastifyCors } from "@fastify/cors";
 import { env } from "./env.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -21,12 +21,4 @@ app.get("/health", () => {
   return { status: "ok" };
 });
 
-app
-  .listen({ port: env.PORT })
-  .then(() => {
-    console.log("HTTP server running on http://localhost:3333");
-  })
-  .catch((err) => {
-    console.error("Error starting server:", err);
-    process.exit(1);
-  });
+app.listen({ port: env.PORT });
