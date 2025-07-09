a008f059d18ce65e9f38c85d29427e97d838378e
Thiago Leite <thiagofmleite@gmail.com>
2025-07-09 14:42:12 -0300

    feat: enhance getRoomsRoute to include questions count and join with questions table

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,3 +1,4 @@
+import { count, eq } from "drizzle-orm";
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
@@ -10,8 +11,11 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
         name: schema.rooms.name,
         description: schema.rooms.description,
         createdAt: schema.rooms.createdAt,
+        questionsCount: count(schema.questions.id),
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id, schema.rooms.name)
       .orderBy(schema.rooms.createdAt);
 
     return results;
