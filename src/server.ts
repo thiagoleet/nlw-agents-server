93978ec7bc5d5ff4233ac9a25512cc737b4c3585
Thiago Leite <thiagofmleite@gmail.com>
2025-07-09 10:51:47 -0300

    feat: implement getRooms route to fetch room details from the database

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { env } from "./env.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -20,5 +21,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.get("/health", () => {
   return { status: "ok" };
 });
+
+app.register(getRoomsRoute);
 
 app.listen({ port: env.PORT });
