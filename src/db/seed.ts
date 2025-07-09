fe44a46a0f9d90ccb2ef359ca286c420a481c505
Thiago Leite <thiagofmleite@gmail.com>
2025-07-09 14:29:35 -0300

    feat: update seed data to include 20 questions for database seeding

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -14,9 +14,9 @@ await seed(db, schema).refine((f) => {
         name: f.companyName(),
         description: f.loremIpsum(),
       },
-      with: {
-        questions: 5,
-      },
+    },
+    questions: {
+      count: 20,
     },
   };
 });
